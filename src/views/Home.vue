a3a274ab5f9368053fd80ee7f12bcc72b380cfe6
Rux <keyboardfox@hotmail.com>
2020-04-01 00:49:45 +0200

    WIP: Need to sleep

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+    <main role="main" class="col-md-9 col-lg-9 ml-auto px-4">
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+      <h1>This is a home page</h1>
+
+    </main>
   </div>
 </template>
