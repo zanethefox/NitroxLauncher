2ecdf9ed1497e8233d99b01e4e653be8099a2668
Rux <keyboardfox@hotmail.com>
2020-04-02 16:19:49 +0200

    Server page

--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Server.vue')
   },
-  {
-      path: '/server-console',
-      name: 'ServerConsole',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/ServerConsole.vue')
-    }
 ]
 
 const router = new VueRouter({
