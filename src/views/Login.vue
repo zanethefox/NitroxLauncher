4808ab08f9d6bfa42b83172d3bff19b9a2e2caf0
Rux <keyboardfox@hotmail.com>
2020-10-02 10:14:41 +0200

    Small tweaks

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,7 @@
 <div class="login">
 
     <main role="main" class="col-md-12 px-4 mt-4 d-flex justify-content-center" style="height: 100vh;">
-      <Particles
-   id="tsparticles"
-   :options="{
+        <Particles id="tsparticles" :options="{
          background: {
              color: {
                  value: ''
@@ -60,8 +58,13 @@
                  value: 20
              },
              opacity: {
-                 value: 0.5
-             },
+
+      animation: {
+        enable: true,
+        minimumValue: 0,
+        speed: 2,
+        sync: false}
+      },
              shape: {
                  type: 'circle'
              },
@@ -71,41 +74,40 @@
              }
          },
          detectRetina: true
-     }"
- />
+     }" />
         <div class="row px-3">
             <div class="col-md-12">
-                    <transition name="fade-in" mode="out-in">
-                        <!-- Login Dialog -->
-                        <div class="col-md-5 m-auto my-auto">
-                            <div class="bg-material-1 p-5 rounded-lg">
-                              <h3>Login</h3>
-                              <p class="opacity-75 mb-4">Sign in to sync your friends, profile and get the best experience.</p>
-                              <button class="btn btn-secondary btn-lg text-uppercase btn-block button-fancy text-left font-14 font-weight-bold py-3" id="button-fancy" v-on:mousemove="updateCoordinates">
-                                  <span class="login steam p-4">
-                                      <img src="../assets/img/login/steam.svg" alt="Steam" width="24px">
-                                  </span>
-                                  <span>
-                                      Continue with Steam
-                                  </span>
-                              </button>
-                              <button class="btn btn-secondary login btn-lg text-uppercase btn-block button-fancy text-left font-14 font-weight-bold py-3" id="button-fancy-2" v-on:mousemove="updateCoordinates2">
-                                  <span class="login epic p-4">
+                <transition name="fade-in" mode="out-in">
+                    <!-- Login Dialog -->
+                    <div class="col-md-5 m-auto my-auto">
+                        <div class="bg-material-3 p-5 rounded-lg">
+                            <h3>Login</h3>
+                            <p class="opacity-75 mb-4">Sign in to sync your friends, profile and get the best experience.</p>
+                            <button class="btn btn-secondary btn-lg text-uppercase btn-block button-fancy text-left font-14 font-weight-bold py-3" id="button-fancy" v-on:mousemove="updateCoordinates">
+                                <span class="login steam p-4">
+                                    <img src="../assets/img/login/steam.svg" alt="Steam" width="24px">
+                                </span>
+                                <span>
+                                    Continue with Steam
+                                </span>
+                            </button>
+                            <button class="btn btn-secondary login btn-lg text-uppercase btn-block button-fancy text-left font-14 font-weight-bold py-3" id="button-fancy-2" v-on:mousemove="updateCoordinates2">
+                                <span class="login epic p-4">
                                     <img src="../assets/img/login/epic.png" alt="Epic" height="24px" style="margin: 0 2px;">
-                                  </span>
-                                  <span>
-                                      Continue with Epic
-                                  </span>
-                              </button>
+                                </span>
+                                <span>
+                                    Continue with Epic
+                                </span>
+                            </button>
 
-                              <router-link to="/" class="text-decoration-none">
+                            <router-link to="/" class="text-decoration-none">
                                 <button class="btn btn-link text-white text-decoration-none text-uppercase button-fancy btn-block font-14 font-weight-bold mt-4" id="button-fancy-3" v-on:mousemove="updateCoordinates3">
-                                        Or skip Login
+                                    Or skip Login
                                 </button>
-                              </router-link>
-                            </div>
+                            </router-link>
                         </div>
-                    </transition>
+                    </div>
+                </transition>
             </div>
         </div>
     </main>
@@ -115,39 +117,46 @@
 
 <style media="screen">
 #tsparticles {
-  position: fixed;
-  width: 100%;
-  height: 100%;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    bottom: 0;
 }
-    .login {
-      z-index: 2000;
-      position: relative;
-    }
-    .btn .epic {
-        background-color: #353535!important;
-    }
-    .btn .steam {
-        background-color: #145c8f!important;
-    }
-    .btn .login {
-        margin-left: -1rem;
-        margin-right: 1rem;
-        z-index: 1;
-    }
-    .button-fancy::before {
-      z-index:2;
-    }
-    .fade-in-enter-active,
-    .fade-in-leave-active {
-        transition: all .35s ease-in-out;
-    }
 
-    .fade-in-enter,
-    .fade-in-leave-to {
-        opacity: 0;
-    }
+.login {
+    z-index: 2000;
+    position: relative;
+}
 
+.btn .epic {
+    background-color: #353535 !important;
+}
 
+.btn .steam {
+    background-color: #145c8f !important;
+}
+
+.btn .login {
+    margin-left: -1rem;
+    margin-right: 1rem;
+    z-index: 1;
+}
+
+.button-fancy::before {
+    z-index: 2;
+}
+
+.fade-in-enter-active,
+.fade-in-leave-active {
+    transition: all .35s ease-in-out;
+}
+
+.fade-in-enter,
+.fade-in-leave-to {
+    opacity: 0;
+}
 </style>
 
 <script>
@@ -159,8 +168,7 @@ const {
 
 export default {
     name: 'Login',
-    components: {
-    },
+    components: {},
     data() {
         return {
             isSettingsModalVisible: false,
